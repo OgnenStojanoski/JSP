082203d94e2c30d33b56a01921afb4fc88148c17
janaSerafimovska <62503690+janaSerafimovska@users.noreply.github.com>
2020-08-22 14:11:01 +0200

    Update database

---
+++ JSP/Migrations/202008221210458_Proba1237.cs
@@ -0,0 +1,16 @@
+namespace JSP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Proba1237 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
